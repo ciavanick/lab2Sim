ea929db0a493db93632139baf8e8348f44e5691b
Niccolò Ciavarelli <152533242+ciavanick@users.noreply.github.com>
2024-08-11 16:40:05 +0200

    minor changes

--- build/CMakeFiles/Lab2.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Lab2"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
